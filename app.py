513415d9e18305a90f30ce89e45cee196c44ba3a
Utkrisht Agrawal <rkayush412@gmail.com>
2026-01-23 19:09:10 +0530

    Test app.py added

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-import os
-print(os.listdir())
-
-import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(page_title="Netflix BI Dashboard", layout="wide")
+st.title("Netflix Watch History EDA")
 
-st.title("Netflix Watch History BI Dashboard")
+@st.cache_data
+def load_data():
+    return pd.read_csv("Netflix_Watch_History_Dataset.csv")
 
-# Load dataset
-df = pd.read_csv("Netflix_Watch_History_Dataset.csv")
+df = load_data()
 
-st.success("Dataset loaded successfully!")
-
+st.success("Data loaded successfully!")
+st.write("Total rows:", df.shape[0])
 st.dataframe(df.head())
-
-# ---------------- Chart 1 ----------------
-st.subheader("Total Watch Time by Subscription Type")
-watch_time = df.groupby('subscription_type')['duration_watched(minutes)'].sum()
-st.bar_chart(watch_time)
-
-# ---------------- Chart 2 ----------------
-st.subheader("Average Watch Time by Device")
-avg_watch = df.groupby('device_used')['duration_watched(minutes)'].mean()
-st.bar_chart(avg_watch)
-
-# ---------------- Chart 3 ----------------
-st.subheader("Viewing Frequency per User")
-views = df.groupby('user_id')['date_watched'].count()
-st.bar_chart(views.value_counts())
